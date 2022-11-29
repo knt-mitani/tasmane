f969323d6302387bf36c98f6d4072fe0fd76912d
Kenta-Mitani <ubuntu@ip-172-31-10-125.ap-northeast-1.compute.internal>
2022-11-29 08:47:23 +0000

    TasksController作成

---
+++ app/Models/Task.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+
+class Task extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = [
+        'title',
+        'content',
+        'importance',
+        'deadline',
+        'status',
+    ];
+    
+    /**
+     * この投稿を所有するユーザ。（ Userモデルとの関係を定義）
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
